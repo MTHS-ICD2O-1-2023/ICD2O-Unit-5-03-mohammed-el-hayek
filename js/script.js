33cf11553364db807a5505c83e81416fa2d2310b
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-04-30 02:04:26 +0000

    V5-3.1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function video() {
+
+  //input
+  const age = parseInt(document.getElementById('age').value)
+
+  //process
+  if (age >= 17) {
+    document.getElementById("answer").innerHTML =
+      "You can see G, PG and R rated movies!"
+  }
+  else if (age >= 13) {
+    document.getElementById("answer").innerHTML =
+      "You can see G and PG rated movies."
+  }
+  else if (age >= 5) {
+    document.getElementById("answer").innerHTML =
+      "You can only see G rated movies."
+  }
+  else {
+    document.getElementById("answer").innerHTML =
+      "You are too young to watch any movies."
+  }
+}
